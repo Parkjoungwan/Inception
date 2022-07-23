15947d6cd9035ca0008670af5da58a034e78b6ee
parkjoungwan <whddhs6645@gmail.com>
2022-07-23 14:25:20 +0900

    fin

--- srcs/mariadb/conf/run_db.sh
+++ srcs/mariadb/conf/run_db.sh
@@ -8,6 +8,10 @@ mv tmp/50-server.cnf /etc/mysql/mariadb.conf.d/
 
 # run mysql
 service mysql start
-mysql < /tmp/dbsql.sql
+mysql -e "create database wordpress_db;"
+mysql -e "create user 'superuser'@'%' identified by '$MYSQL_PASSWORD';"
+mysql -e "grant all privileges on *.* to 'superuser'@'%';"
+mysql -e "create user '$MYSQL_USER'@'%' identified by '$MYSQL_PASSWORD';"
+mysql -e "grant all on wordpress_db.* to '$MYSQL_USER'@'%';"
 
 bash
